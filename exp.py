dbcfe69cd72c656733aa9c5b089d3b3b266ec115
SuvenduMullick2023 <m22aie218@iitj.ac.in>
2023-09-07 17:58:20 +0530

    code updated for assignment3

--- exp.py
+++ exp.py
@@ -11,122 +11,42 @@ hand-written digits, from 0-9.
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
-# Standard scientific Python imports
-import matplotlib.pyplot as plt
-
 # Import datasets, classifiers and performance metrics
-from sklearn import datasets, metrics, svm
-from sklearn.model_selection import train_test_split
+from sklearn import metrics, svm
+from utils import preprocess_data, read_digits, predict_and_eval, split_train_dev_test ,tune_hparams
 
+# 1. Get the dataset
+X, y = read_digits()
 
-def split_train_dev_test(X, y, test_size, dev_size):
-    # Split data into 60% train and 20% test subsets and 20% validation
-    X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=test_size, shuffle=False)
-    X_train, X_dev, y_train, y_dev = train_test_split(
-    X_train, y_train, test_size=dev_size, shuffle=False)
-    return X_train,X_test,X_dev,y_train,y_test,y_dev
+# 3. Data splitting -- to create train and test sets
 
-def predict_and_eval(model, X_test, y_test):
-    predicted=model.predict(X_test)
+# X_train, X_test, y_train, y_test = split_data(X,y, test_size=0.3)
+test_size = [0.1, 0.2, 0.3]
+dev_size = [0.1, 0.2, 0.3]
+for i in test_size :
+    for j in dev_size : 
+        X_train, X_test,X_dev, y_train, y_test, y_dev = split_train_dev_test(X, y, test_size=i, dev_size=j)
+        
+        # 4. Data preprocessing
+        X_train = preprocess_data(X_train)
+        X_dev = preprocess_data(X_dev)
+        X_test = preprocess_data(X_test)
+        
+        
+        
+        
+        
+        
+        
+        
+        # 5. Model training
+        param_dict = {'gamma': [0.001,0.01,0.1,1],'C' : [1.0,10.0,20.0],'kernels' :['rbf','linear']}
+        
+        model = tune_hparams( X_train, y_train,X_dev, y_dev,param_dict)
 
-    # 7. Qualitative sanity check of the prediction
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(8, 8)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title(f"Prediction: {prediction}")
+        print('Train : {0} Test_size : {1} Dev_size :{2}'.format( round(1-i-j,1) , i,  j)," ", end = "")
 
-    ###############################################################################
-    # :func:`~sklearn.metrics.classification_report` builds a text report showing
-    # the main classification metrics.
-
-    # 8. Evaluation
-    print(
-        f"Classification report for classifier {model}:\n"
-        f"{metrics.classification_report(y_test, predicted)}\n"
-    )
-    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-    disp.figure_.suptitle("Confusion Matrix")
-    print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-    plt.show()
-
-    # The ground truth and predicted lists
-    y_true = []
-    y_pred = []
-    cm = disp.confusion_matrix
-
-    # For each cell in the confusion matrix, add the corresponding ground truths
-    # and predictions to the lists
-
-    
-    for gt in range(len(cm)):
-        for pred in range(len(cm)):
-            y_true += [gt] * cm[gt][pred]
-            y_pred += [pred] * cm[gt][pred]
-
-    ###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit val
-# ues and the predicted digit values.
-    print(
-        "Classification report rebuilt from confusion matrix:\n"
-        f"{metrics.classification_report(y_true, y_pred)}\n"
-    )
-
-# 1. get the data set 
-digits = datasets.load_digits()
-
-#2. Qualitative sanity check of the data
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
-
-###############################################################################
-
-
-# 3. Data Pre processing 
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
-
-X = data
-y = digits.target
-#4. Data spliting to create traing and test set 
-# Split data into 60% train and 20% test subsets and 20% validation
-test_size ,dev_size = 0.2 ,0.2
-
-
-X_train, X_test,X_dev, y_train, y_test,y_dev = split_train_dev_test(X,y,test_size ,dev_size)
- 
-
-
-# 5. Model Training 
-# Create a classifier: a support vector classifier
-model = svm.SVC(gamma=0.001)
-
-# Learn the digits on the train subset
-model.fit(X_train, y_train)
-
-
-#6. getting the model prediction on the test set
- 
-# Predict the value of the digit on the test subset
-predict_and_eval(model,X_test,y_test)
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # 6. Getting model predictions on test set
+        # 7. Qualitative sanity check of the predictions
+        # 8. Evaluation
+        predict_and_eval(model, X_test, y_test)
--- utils.py
+++ utils.py
@@ -1,20 +1,18 @@
 from sklearn.model_selection import train_test_split
-from sklearn import svm, datasets, metrics
+from sklearn import svm, datasets, metrics 
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+import itertools  
+from sklearn.metrics import accuracy_score
+#import  matplotlib as plt
 # we will put all utils here
-# Standard scientific Python imports
-import matplotlib.pyplot as plt
+
 
 
 def read_digits():
     digits = datasets.load_digits()
     X = digits.images
     y = digits.target
-    #2. Qualitative sanity check of the data
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, label in zip(axes, digits.images, digits.target):
-        ax.set_axis_off()
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title("Training: %i" % label)
     return X, y 
 
 def preprocess_data(data):
@@ -23,43 +21,27 @@ def preprocess_data(data):
     data = data.reshape((n_samples, -1))
     return data
 
-# Split data into 50% train and 50% test subsets
-def split_data(x, y, test_size, random_state=1):
-    X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.5,random_state=random_state
-    )
-    return X_train, X_test, y_train, y_test
-
-# train the model of choice with the model prameter
-def train_model(x, y, model_params, model_type="svm"):
-    if model_type == "svm":
-        # Create a classifier: a support vector classifier
-        clf = svm.SVC
-    model = clf(**model_params)
-    # train the model # Learn the digits on the train subset
-    model.fit(x, y)
-    return model
 
 
-
-
-def split_train_dev_test1(X, y, test_size, dev_size):
+def split_train_dev_test(X, y, test_size, dev_size):
     # Split data into 60% train and 20% test subsets and 20% validation
-    X_train_dev, X_test, y_train_dev, y_test = train_test_split(
-    X, y, test_size=test_size, shuffle=False,random_state=1)
-    X_train, X_dev, y_train, y_dev = train_test_split(
-    X_train_dev, y_train_dev, test_size=dev_size, shuffle=False,random_state=1)
+    X_train_dev, X_test, y_train_dev, y_test = train_test_split(X, y, test_size=test_size, shuffle=False)
+    X_train, X_dev, y_train, y_dev = train_test_split(X_train_dev, y_train_dev, test_size=dev_size, shuffle=False)
     return X_train,X_test,X_dev,y_train,y_test,y_dev
 
+
+
+# Question 2:
 def predict_and_eval(model, X_test, y_test):
     predicted = model.predict(X_test)
-    print(
+    '''print(
     f"Classification report for classifier {model}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
-    )
-
-
-    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    )'''
+    accuracy_test = accuracy_score(predicted, y_test)
+    print("Accuracy_test  : {0:2f}%".format(accuracy_test*100))
+    print("-----------------------------------------------------------")
+    '''disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
     disp.figure_.suptitle("Confusion Matrix")
     print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
@@ -79,53 +61,50 @@ def predict_and_eval(model, X_test, y_test):
     print(
         "Classification report rebuilt from confusion matrix:\n"
         f"{metrics.classification_report(y_true, y_pred)}\n"
-    )
-
-def predict_and_eval1(model, X_test, y_test):
-    predicted=model.predict(X_test)
-
-    # 7. Qualitative sanity check of the prediction
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(8, 8)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title(f"Prediction: {prediction}")
-
-    ###############################################################################
-    # :func:`~sklearn.metrics.classification_report` builds a text report showing
-    # the main classification metrics.
-
-    # 8. Evaluation
-    print(
-        f"Classification report for classifier {model}:\n"
-        f"{metrics.classification_report(y_test, predicted)}\n"
-    )
-    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-    disp.figure_.suptitle("Confusion Matrix")
-    print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-    plt.show()
-
-    # The ground truth and predicted lists
-    y_true = []
-    y_pred = []
-    cm = disp.confusion_matrix
-
-    # For each cell in the confusion matrix, add the corresponding ground truths
-    # and predictions to the lists
-
+    )'''
+def tune_hparams( X_train, Y_train,x_dev, y_dev,list_of_all_param_combination):
     
-    for gt in range(len(cm)):
-        for pred in range(len(cm)):
-            y_true += [gt] * cm[gt][pred]
-            y_pred += [pred] * cm[gt][pred]
-
-    ###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit val
-# ues and the predicted digit values.
-    print(
-        "Classification report rebuilt from confusion matrix:\n"
-        f"{metrics.classification_report(y_true, y_pred)}\n"
-    )
+    keys, values = zip(*list_of_all_param_combination.items())
+    permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    #print("The combinations dictionary : " + str(permutations_dicts))
+    
+    
+    # default setting     
+    g = 'scale'
+    cval = 1.0
+    kval = 'rbf'
+    best_score =[0,0]
+    best_model = None
+    avg_scores ={}
+    for dict1 in permutations_dicts :
+        #print(dict1)
+        for k,v in dict1.items():
+                    
+                    if k == 'gamma':
+                        g = v
+                    elif k =='C':
+                        cval = v
+                    elif k == 'kernels':
+                        kval = v
+        #print(kval,cval,g)    
+        cur_model =svm.SVC(kernel =kval,C=cval,gamma= g)
+        cur_model.fit(X_train, Y_train)
+        cv_scores = cur_model.score(x_dev, y_dev)
+        avg_scores[str(kval) + "-" + str(cval)+ "-" + str(g)]= round(np.average(cv_scores),3)
+        
+        if best_score[1] < round(np.average(cv_scores),3) :
+            best_model = cur_model
+            best_score[1] = round(np.average(cv_scores),3)
+            best_score[0] = str(kval) + "-" + str(cval)+ "-" + str(g)
+            
+            y_pred_train =best_model.predict(X_train)
+            accuracy_train = accuracy_score(y_pred_train, Y_train)
+            y_pred_dev =best_model.predict(x_dev)
+            accuracy_dev = accuracy_score(y_pred_dev, y_dev)
+            
+    #print('All_scores :' ,avg_scores)
+    print("Optimal parameters ::: Kernel--C--gamma  : ", best_score)
+    print("Accuracy Train : {0:2f}%  Accuracy_Dev :{1:2f}%".format(accuracy_train*100, accuracy_dev*100 ))
+    return best_model
+    
+    
