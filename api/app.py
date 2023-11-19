7d8111fa89a6d63149c6b2a16535a775f4f22a80
SuvenduMullick2023 <m22aie218@iitj.ac.in>
2023-11-19 17:08:45 +0530

    quize4

--- api/app.py
+++ api/app.py
@@ -4,13 +4,21 @@ import numpy as np
 from PIL import Image
 import io ,os
 
+app = Flask(__name__)
 
+@app.route("/")
+def hello_world1():
+    return "<p>Hello, World!</p>"
+
+@app.route("/", methods=["POST"])
+def hello_world_post():    
+    return {"op" : "Hello, World POST " + request.json["suffix"]}
 
 @app.route("/predict", methods = ['POST'])
 def pred_image():
     js = request.get_json( )
     image1 = js['image']
-    best_model = load('./models/svm_gamma:0.001_C:1.joblib')
+    best_model = load('./models/svm_gamma_0.001_C_1_kernel_rbf.joblib')
     image1_1d = np.array(image1).reshape(1, -1)
     predicted1 = best_model.predict(image1_1d)
     print(predicted1)
